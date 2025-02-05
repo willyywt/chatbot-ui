177cc34dffcda2603bb0815fcd1d227a3c0fb75b
willyang <22425315+willyywt@users.noreply.github.com>
2025-02-05 19:41:41 +0800

    Change model to GPT-4o mini

--- types/openai.ts
+++ types/openai.ts
@@ -1,29 +1,25 @@
 export interface OpenAIModel {
   id: string;
   name: string;
+  // Didn't understand what does this do. Currently always set to 3 * tokenLimit
   maxLength: number; // maximum length of a message
   tokenLimit: number;
 }
 
+// See https://platform.openai.com/docs/models
+// It doesn't makes sense to use GPT-4 and GPT-3.5 now. They are more expensive but less performant
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_4 = 'gpt-4',
+  GPT_4O_MINI = 'gpt-4o-mini',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.GPT_4O_MINI;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-  },
-  [OpenAIModelID.GPT_4]: {
-    id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
-    maxLength: 24000,
-    tokenLimit: 8000,
+  [OpenAIModelID.GPT_4O_MINI]: {
+    id: OpenAIModelID.GPT_4O_MINI,
+    name: 'GPT-4o Mini',
+    maxLength: 384000,
+    tokenLimit: 128000,
   },
 };
